6ec065704763226a842431948d1de31f774052d7
Paul Backhouse <paulcbackhouse@gmail.com>
2019-08-30 14:11:56 +0100

    Remove non-exact matches from text search results.

--- src/trakt_api.py
+++ src/trakt_api.py
@@ -29,18 +29,19 @@ def post_films(films):
         if film.get('imdb'):
             logger.info('Using imdb reference to get trakt id')
             response = requests.get(SEARCH_IMDB_URL % film['imdb'], headers=headers)
+            results = response.json()
         else:
             logger.info('No imdb reference provided. Using text search.')
             params = {'query' : film['title'], 'fields':'title'}
             response = requests.get(SEARCH_TEXT_URL, params=params, headers=headers)
+            # Remove non-exact matches. This relies in the film title being absolutely correct.
+            results = [x for x in response.json() if x['movie']['title'] == film['title']]
 
-        # Filter out non-exact matches.
-        results = [x for x in response.json() if x['score'] == 1000]
+            # sort by release date, assuming that the review is for the latest film with that title.
+            # The slug contains the release date, eg 'the-nightingale-2019-01-25' and appears more
+            # more likely to exist than ['movie']['year'].
+            results = sorted(results, key=lambda k: k['movie']['ids']['slug'], reverse=True)
 
-        # sort by release date, assuming that the review is for the latest film with that title.
-        # The slug contains the release date, eg 'the-nightingale-2019-01-25' and appears more
-        # more likely to exist than ['movie']['year'].
-        results = sorted(results, key=lambda k: k['movie']['ids']['slug'], reverse=True)
         trakt_id = results[0]['movie']['ids']['trakt']
         trakt_ids.add(trakt_id)
 
