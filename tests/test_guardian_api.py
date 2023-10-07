7134636c5f0e310704bfc0e36b20f62b5d52988b
powlo <paulcbackhouse@gmail.com>
2023-10-08 00:20:17 +0100

    Fixup tests.

--- tests/test_guardian_api.py
+++ tests/test_guardian_api.py
@@ -12,10 +12,18 @@ class MockAPI(object):
 @mock.patch("guardian_api.get_secret", lambda _: {"API_KEY": "123"})
 @mock.patch("guardian_api.requests.get")
 class TestGuardianAPI(unittest.TestCase):
-    def test_simple(self, mock_get):
+    def test_get_films(self, mock_get):
         mock_get.return_value.json.return_value = {
-            "response": {"results": [{"webTitle": "a film review"}]}
+            "response": {
+                "results": [{"webTitle": "a film review", "webUrl": "www.aurl.com"}],
+                "pages": 1,
+            }
         }
         films = guardian_api.get_films()
         self.assertEqual(1, len(films))
+        self.assertEqual({"title": "a film"}, films[0])
+
+    def test_parse_results(self, mock_get):
+        results = [{"webTitle": "a film review", "webUrl": "www.aurl.com"}]
+        films = guardian_api.parse_results(results)
         self.assertEqual({"title": "a film"}, films[0])
