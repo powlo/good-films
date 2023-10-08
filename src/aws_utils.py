c8bd1ebb54c6ba7177a541376b22c50bd06c6bfb
powlo <paulcbackhouse@gmail.com>
2023-10-08 17:14:23 +0100

    Import boto3 session properly.

--- src/aws_utils.py
+++ src/aws_utils.py
@@ -9,14 +9,14 @@ def get_secret(secret_id):
     """
     Helper function to get the contents of a given secret.
     """
-    session = boto3.session.Session()
+    session = boto3.Session()
     client = session.client(service_name="secretsmanager", region_name="eu-west-2")
     response = client.get_secret_value(SecretId=secret_id)
     return json.loads(response["SecretString"])
 
 
 def put_secret(secret_id, secret_values):
-    session = boto3.session.Session()
+    session = boto3.Session()
     client = session.client(service_name="secretsmanager", region_name="eu-west-2")
     return client.update_secret(
         SecretId=secret_id,
