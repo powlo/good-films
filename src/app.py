893ce75db277396345cc08cc55407ebbb81c6ebd
powlo <paulcbackhouse@gmail.com>
2025-03-25 16:00:03 +0000

    Tweak app logging.

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 from datetime import datetime
 
 import guardian_api
@@ -13,13 +14,14 @@ def lambda_handler(event, context):
     # The date the last time the script ran is stored in a parameter.
     # So days are not lost if the script fails for any reason.
     last_success = get_parameter("GoodFilms_LastSuccess")
+    last_success = "2025-01-19"
     from_date = datetime.strptime(last_success, "%Y-%m-%d")
 
     for films in guardian_api.get_films(from_date):
         if films:
             logger.info("Found %s film reviews." % len(films))
             for film in films:
-                logger.info(f'"{film["webTitle"]}" (f{film["webUrl"]})')
+                logger.info(f'"{film["webTitle"]}" ({film["webUrl"]})')
             ids = guardian_api.extract_imdb_ids(films)
         else:
             logger.info("No film reviews found.")
@@ -33,4 +35,5 @@ def lambda_handler(event, context):
 
 
 if __name__ == "__main__":
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
     lambda_handler(None, None)
