0309f4238d78e2a96ec8d456dd8393bc7b379c2a
powlo <paulcbackhouse@gmail.com>
2025-03-28 17:01:40 +0000

    Code restructure/tidy.
    Make an API class with routes.
    Easier to read. Makes testing a bit easier.

--- src/trakt_api.py
+++ src/trakt_api.py
@@ -6,69 +6,86 @@ import requests
 
 from aws_utils import get_secret
 
-BASE_URL = "https://api.trakt.tv"
-SEARCH_TEXT_URL = BASE_URL + "/search/movie"
-SEARCH_IMDB_URL = BASE_URL + "/search/imdb/%s"
-USER_SETTINGS_URL = BASE_URL + "/users/settings"
-LIST_URL = BASE_URL + "/users/%s/lists/%s/items" % ("ukdefresit", "guardian-films")
-MAX_LIST_SIZE = 100
-
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-def get_headers():
-    secrets = get_secret("TraktAPI")
-    return {
-        "Content-Type": "application/json",
-        "trakt-api-version": "2",
-        "Authorization": f"Bearer {secrets['ACCESS_TOKEN']}",
-        "trakt-api-key": secrets["CLIENT_ID"],
-    }
+class ListRoute:
+    def __init__(
+        self, session: requests.Session, base_url: str, user_id: str, list_id: str
+    ):
+        self.session = session
+        # user_id is the username of the person who owns the list.
+        self.user_id = user_id
+        # list_id is the name of the list.
+        self.list_id = list_id
+        self.base_url = base_url + f"/users/{user_id}/lists/{list_id}/items"
+
+    def get(self, category="movies", sort_by="added", sort_order="desc"):
+        url = self.base_url + f"/{category}/{sort_by}/{sort_order}"
+        response = self.session.get(url)
+        # Raise exception if no bueno.
+        response.raise_for_status()
+        return response.json()
+
+    def add(self, imdb_ids: Set[str]):
+        data = {"movies": [{"ids": {"imdb": imdb_id}} for imdb_id in imdb_ids]}
+
+        # TODO: We need to handle situation where we're trying to post continuously.
+        # response will contain a "retry-after" in the header. Maybe use that.
+        response = self.session.post(self.base_url, data=json.dumps(data))
+        response.raise_for_status()
+        return response.json()
+
+    def delete(self, imdb_ids: Set[str]):
+        url = self.base_url + "/remove"
+        data = {"movies": [{"ids": {"imdb": imdb_id}} for imdb_id in imdb_ids]}
+        response = self.session.post(url, data=json.dumps(data))
+        response.raise_for_status()
+        return response.json()
 
 
-def search_by_exact_title(title):
-    # TODO: Remove.
-    """
-    Convenience wrapper around trakt api to search by exact title matches.
-    """
-    # NB We put the film title in double quotes to perform an exact match.
-    params = {"query": '"%s"' % title, "fields": "title"}
-    headers = get_headers()
-    return requests.get(SEARCH_TEXT_URL, params=params, headers=headers).json()
+class SearchRoute:
+    def __init__(self, session: requests.Session, base_url: str):
+        self.session = session
+        self.base_url = base_url + "/search"
+
+    def by_text(self, text: str, fields="title"):
+        url = self.base_url + "/movie"
+        params = {"query": '"%s"' % text, "fields": fields}
+        response = self.session.get(url, params=params)
+        response.raise_for_status()
+        return response.json()
+
+    def by_id(self, id: str, id_type="imdb", type="movie"):
+        url = self.base_url + f"/{id_type}/{id}?type={type}"
+        response = self.session.get(url)
+        response.raise_for_status()
+        return response.json()
 
 
-def search_by_imdb_id(id):
-    """
-    Convenience wrapper around trakt api to search by imdb id .
-    """
-    # TODO: Remove? Seems we can add and delete films directly using imdb id.
-    headers = get_headers()
-    return requests.get(SEARCH_IMDB_URL % id, headers=headers).json()
+class TraktAPI:
+    def __init__(
+        self, client_id: str, access_token: str, base_url="https://api.trakt.tv"
+    ):
+        session = requests.Session()
+        session.headers.update(
+            {
+                "Content-Type": "application/json",
+                "trakt-api-version": "2",
+                "Authorization": f"Bearer {access_token}",
+                "trakt-api-key": client_id,
+            }
+        )
+        self.session = session
+        self.base_url = base_url
 
+    def list(self, user_id: str, list_id: str):
+        return ListRoute(self.session, self.base_url, user_id, list_id)
 
-def list_get(category="movies", sort_by="added", sort_order="asc"):
-    headers = get_headers()
-    # NB the documented technique of adding "X-Sort-By" headers does not seem to work.
-    # So we do it as part of the url.
-    LIST_GET_URL = LIST_URL + f"/{category}/{sort_by}/{sort_order}"
-    return requests.get(LIST_GET_URL, headers=headers)
-
-
-def list_delete(imdb_ids):
-    headers = get_headers()
-    LIST_DELETE_URL = LIST_URL + "/remove"
-    data = {"movies": [{"ids": {"imdb": imdb_id}} for imdb_id in imdb_ids]}
-    return requests.post(LIST_DELETE_URL, data=json.dumps(data), headers=headers)
-
-
-def list_add(imdb_ids):
-    headers = get_headers()
-    data = {"movies": [{"ids": {"imdb": imdb_id}} for imdb_id in imdb_ids]}
-
-    # TODO: We need to handle situation where we're trying to post continuously.
-    # response will contain a "retry-after" in the header. Maybe use that.
-    return requests.post(LIST_URL, data=json.dumps(data), headers=headers)
+    @property
+    def search(self):
+        return SearchRoute(self.session, self.base_url)
 
 
 def update_list(imdb_ids: Set[str]):
@@ -76,10 +93,14 @@ def update_list(imdb_ids: Set[str]):
     Takes a set of ids that can be recognised by IMDB (eg tt0076759)
     and POSTs them to trakt.
     """
-
-    response = list_get(sort_by="added", sort_order="desc")
-    list_items = response.json()
-    excess = max(len(list_items) + len(imdb_ids) - MAX_LIST_SIZE, 0)
+    secrets = get_secret("TraktAPI")
+    user_id = secrets["USER_ID"]
+    list_id = secrets["LIST_ID"]
+    max_list_size = int(secrets["MAX_LIST_SIZE"])
+    api = TraktAPI(secrets["CLIENT_ID"], secrets["ACCESS_TOKEN"])
+    api_list = api.list(user_id, list_id)
+    list_items = api_list.get()
+    excess = max(len(list_items) + len(imdb_ids) - max_list_size, 0)
     if excess:
         logger.warning(
             "Too many items in the list. List will be truncated to make room."
@@ -88,22 +109,17 @@ def update_list(imdb_ids: Set[str]):
         imdb_ids_to_delete = set(
             [item["movie"]["ids"]["imdb"] for item in items_to_delete]
         )
-        response = list_delete(imdb_ids_to_delete)
+        response = api_list.delete(imdb_ids_to_delete)
         info = response.json()
         logger.info("Deleted %s from list." % info["deleted"]["movies"])
         logger.info("List now contains %s items." % info["list"]["item_count"])
 
-    response = list_add(imdb_ids)
-    if response.ok:
-        logger.info("Successfully added %d films to trakt list." % len(imdb_ids))
-    else:
+    try:
+        result = api_list.add(imdb_ids)
+        logger.info(
+            "Successfully added %d films to trakt list." % result["added"]["movies"]
+        )
+    except requests.exceptions.JSONDecodeError:
+        # We probably want to handle this better.
+        # Ie catch when the request fails and (re)raise something appropriate.
         logger.error("Failed to add films to trakt list.")
-        logger.info("URL: %s" % response.url)
-        logger.info("Reason: (%s) %s" % (response.status_code, response.reason))
-
-
-# Mostly for debug purposes.
-def get_user_settings():
-    headers = get_headers()
-    response = requests.get(USER_SETTINGS_URL, headers=headers)
-    print(response)
