a1981131edd53cd81165a1d246990d72e4254fe5
powlo <paulcbackhouse@gmail.com>
2026-05-04 17:00:18 +0100

    Tweak guardian handler to send json to message queue.

--- guardian/app.py
+++ guardian/app.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 from datetime import datetime
@@ -5,8 +6,7 @@ from typing import Iterator
 
 import boto3
 import requests
-
-from .aws_utils import get_parameter, get_secret, put_parameter
+from aws_utils import get_parameter, get_secret, put_parameter
 
 BASE_URL = "https://content.guardianapis.com"
 SEARCH_URL = BASE_URL + "/search"
@@ -23,6 +23,7 @@ def get_articles(from_date: datetime) -> Iterator[dict]:
     current_page = 1
     pages = 1
     from_date_string = from_date.strftime("%Y-%m-%d")
+    logger.info(f"Fetching articles from {from_date_string}")
     while current_page <= pages:
         params = {
             "api-key": get_secret("GuardianAPI")["API_KEY"],
@@ -35,16 +36,18 @@ def get_articles(from_date: datetime) -> Iterator[dict]:
             "page": current_page,
         }
         # gets raw articles from guardian.
+        logger.info(f"Fetching page {current_page} of results.")
         response = requests.get(SEARCH_URL, params=params)
         json_data = response.json()
         pages = json_data["response"]["pages"]
         results = json_data["response"]["results"]
+        logger.info(f"Got {len(results)} results from {SEARCH_URL}.")
         for data in results:
             yield data
         current_page += 1
 
 
-def lambda_handler(event, context):
+def lambda_handler(event=None, context=None):
     # The date the last time the script successfully ran is stored in a parameter.
     # So days are not lost if the script fails for any reason.
     last_success = get_parameter("GoodFilms_LastSuccess")
@@ -53,6 +56,12 @@ def lambda_handler(event, context):
     from_date = datetime.strptime(last_success, DATE_FORMAT)
 
     for data in get_articles(from_date):
-        sqs.send_message(QueueUrl=GUARDIAN_ARTICLE_QUEUE_URL, MessageBody=data)
+        sqs.send_message(
+            QueueUrl=GUARDIAN_ARTICLE_QUEUE_URL, MessageBody=json.dumps(data)
+        )
     now = datetime.now()
     put_parameter("GoodFilms_LastSuccess", now.strftime(DATE_FORMAT))
+
+
+if __name__ == "__main__":
+    lambda_handler()
